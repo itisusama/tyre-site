b0891eff0b409e44b332340a13a4f5de5adf14f8
Usama Hassan <ahmarkhan1233456@gmail.com>
2024-02-16 10:51:23 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../src/assets/bootstrap/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <button className='btn btn-primary'>Button</button>
+    </div>
+  )
+}
+
+export default App
