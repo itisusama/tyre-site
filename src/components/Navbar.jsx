5e1b6101e7dbfa62d0e46ef8e3c9bdfbbb167e2e
Usama Hassan <ahmarkhan1233456@gmail.com>
2024-02-26 11:55:40 +0500

    nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,9 +16,9 @@ const Navbar = () => {
 
        <div className="container-fluid position-relative p-0">
         <nav className="navbar navbar-expand-lg navbar-light bg-dark px-4 px-lg-5 py-2 py-lg-0">
-          <a href="" className="navbar-brand p-0">
+          <Link to="/" className="navbar-brand p-0">
             <h1 className="m-0">Tyre Site</h1>
-          </a>
+          </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span className="fa fa-bars"></span>
           </button>
